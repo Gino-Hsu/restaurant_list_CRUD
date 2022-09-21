9a017d8fe0638aa62afdd1fae00e18b40d17be22
GinoHsu <ginohsu16@gmail.com>
2022-09-22 00:01:04 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const Restaurant = require('./models/restaurant')
 const routes = require('./routes') // 引用路由器
 
-const mongoose = require('mongoose') //載入 mongoose
-const restaurant = require('./models/restaurant')
-mongoose.connect(process.env.MONGODB_restaurant_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+require('./config/mongoose')
 
 // setting template engine
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
@@ -21,17 +18,6 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes) // 將 request 導入路由器
-
-// 取得資料庫連線資料
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,18 +1,10 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json').results //載入種子資料
 
-mongoose.connect(process.env.MONGODB_restaurant_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('../../config/mongoose')
 
-// 取得資料庫連線資料
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 // 連線成功
 db.once('open', () => {
-  console.log('mongodb connected!')
   // 建立種子資料
   Restaurant.create(restaurantList)
     .then(() => {
